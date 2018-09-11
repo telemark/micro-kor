4386a92d06c9963b830710431abf5e5b11bfb3b6
Jonas Enge <jonas.enge@gmail.com>
2018-09-11 10:23:02 +0200

    Standard fixes

--- src/lib/handle-unauthorized.js
+++ src/lib/handle-unauthorized.js
@@ -1,9 +1,9 @@
-const {send} = require('micro')
+const { send } = require('micro')
 const logger = require('./logger')
 
 module.exports = (error, request, response, next) => {
   if (error.name === 'UnauthorizedError') {
     logger('error', ['handle-unauthorized', error])
-    send(response, 401, {error: 'invalid token'})
+    send(response, 401, { error: 'invalid token' })
   }
 }
